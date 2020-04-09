93221427c02aafe3b98350ac7d7015bd8ab2f38f
Larissa Araújo <lariaraujo201@gmail.com>
2020-04-09 10:57:58 -0300

    redux persist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <Provider store={store}>
-      <TodoList />
+      <PersistGate loading={null} persistor={persistor}>
+        <TodoList />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,17 @@
 import { createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './ducks';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'todo',
+  storage
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
